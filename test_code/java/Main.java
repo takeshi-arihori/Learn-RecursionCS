3974b5b4865be60823aeeee1d890956ba88b54f4
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-02-01 07:35:58 +0900

    2024-01-31_daily-log

--- test_code/java/Main.java
+++ test_code/java/Main.java
@@ -1,28 +1,29 @@
 package test_code.java;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class Main {
 
-    public static long fibonacci(int n) {
-
-        if (n == 0) {
-            return 0;
+    public static ArrayList<ArrayList<Integer>> getDivisorCombinationList(int n) { // 10
+        ArrayList<ArrayList<Integer>> divisor = new ArrayList<>();
+        for (int i = 1; i <= Math.ceil(Math.sqrt(n)); i++) {
+            ArrayList<Integer> combination = new ArrayList<>();
+            if (n % i != 0) {
+                continue;
+            }
+            combination.add(i);
+            combination.add(n / i);
+            divisor.add(combination);
         }
+        return divisor;
+    }
 
-        if (n == 1) {
-            return 1;
-        }
-
-        long[] cache = new long[n + 1];
-        cache[0] = 0;
-        cache[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            cache[i] = cache[i - 1] + cache[i - 2];
-        }
-
-        return cache[n];
+    public static boolean divisorCheck(int k, int num) {
+        // getDivisorCombinationList関数の整合性をチェックしてください
+        ArrayList<ArrayList<Integer>> divisor = getDivisorCombinationList(k);
+        // 重複削除 && kより大きくnumより小さいものを対象とする
+        return divisor.size() == num;
     }
 
     public static void unitTestCheck(boolean predicate) {
@@ -33,13 +34,20 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println("------------- fibonacchテスト ------------");
-        // unitTestCheck(fibonacci(0) == 0);
-        // unitTestCheck(fibonacci(1) == 1);
+    public static void unitTests(int[] inputs, int[] outputs) {
 
-        // List<Integer> list = new ArrayList<Integer>();
-        // System.out.println(list instanceof List);
-        // System.out.println(list instanceof ArrayList);
+        IntStream.range(0, inputs.length).forEach(i -> {
+            int input = inputs[i];
+            int output = outputs[i];
+            boolean predicate = divisorCheck(input, output);
+            unitTestCheck(predicate);
+        });
+    }
+
+    public static void main(String[] args) {
+        // テストケース: unitTests([1,10,120,720,1260,5040,25200],[0,2,8,15,18,30,45])
+        int[] inputs = { 1, 10, 120, 720, 1260, 5040, 25200 };
+        int[] outputs = { 0, 2, 8, 15, 18, 30, 45 };
+        unitTests(inputs, outputs);
     }
 }
