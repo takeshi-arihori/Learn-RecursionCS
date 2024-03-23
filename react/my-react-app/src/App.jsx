209ebc84c05b3c3e82180c53c7620678d4032fc6
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-03-23 10:33:11 +0900

    20240322_daily-log

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,15 +1,14 @@
 import "./App.css";
-import { Form } from "./components/Form";
-import Todos from "./components/Todos";
-import React from "react";
+// import { Form } from "./components/Form";
+// import Todos from "./components/Todos";
 
 export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Todos />
-        <Form />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* <Todos /> */}
+                {/* <Form /> */}
+            </header>
+        </div>
+    );
 }
