b63faf0b41c19aff3a1a8b42148c54b707021ed1
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-04-04 10:47:03 +0900

    20240403_daily-log

--- php/index.php
+++ php/index.php
@@ -1,27 +1,4 @@
 <?php
-
-function splitAndAdd(int $digits): int
-{
-    return splitAndAddHelper($digits, 0);
-}
-
-function splitAndAddHelper(int $digits, int $cnt): int
-{
-    if (!$digits) return $cnt;
-    echo "digits : " . $digits . "<br>";
-    $cnt += $digits % 10;
-    echo "digits % = " . $digits % 10  . "<br>";
-    $digits = (int)($digits / 10);
-    echo "digits / = " . $digits / 10 . "<br>";
-
-    return splitAndAddHelper($digits, $cnt);
-}
-
-?>
-
-<p><?php print(splitAndAdd(19)); ?></p>
-<p><?php print(splitAndAdd(999999)); ?></p>
-<p><?php print(splitAndAdd(123456)); ?></p>
-<p><?php print(splitAndAdd(123456789)); ?></p>
-<p><?php print(splitAndAdd(1234567890)); ?></p>
-<p><?php print(splitAndAdd(12345678901234567890)); ?></p>
+var_dump(is_float(4));
+var_dump(is_float(4.4));
+var_dump(is_float(7.0));
