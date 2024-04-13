db6df1a387447948554a74e4aa2fdee340fb8eea
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-04-13 10:46:43 +0900

    練習用のreactディレクトリ作成 Tailwind.css初期設定完了

---
+++ react/my-react-app-ts/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+    return (
+        <>
+            <h1 className="text-center p-3 text-3xl font-bold underline">Hello Vite & React, TypeScript</h1>
+        </>
+    )
+}
+
+export default App
