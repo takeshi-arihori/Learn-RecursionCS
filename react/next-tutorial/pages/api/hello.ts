5ebcd9a881a65603ad7cbcdd45012a9530e5be36
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-04-30 21:41:55 +0900

    APIルート

--- react/next-tutorial/pages/api/hello.ts
+++ react/next-tutorial/pages/api/hello.ts
@@ -1,13 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
+type HelloResponse = {
   name: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
+// /api/helloで呼ばれた時のAPIの挙動を実装する
+export default (req: NextApiRequest, res: NextApiResponse<HelloResponse>) => {
+  // ステータス200で{"name":"John Doe"}を返す
   res.status(200).json({ name: "John Doe" });
-}
+};
