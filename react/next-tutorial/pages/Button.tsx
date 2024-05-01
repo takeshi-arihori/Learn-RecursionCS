2b2c9b6cae978e42a9cdae4192f74158b34d1946
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-05-01 20:58:40 +0900

    Presentational Component

---
+++ react/next-tutorial/pages/Button.tsx
@@ -0,0 +1,23 @@
+import '@styles/Button.css'
+
+type ButtonProps = {
+	label: string
+	text: string
+	disabled: boolean
+	onClick: React.MouseEventHandler<HTMLButtonElement>
+}
+
+// ラベルとボタンを表示するコンポーネント
+export const Button = (props: ButtonProps) => {
+	const { label, text, disabled, onClick } = props
+
+	// propsで渡されたデータを元に見た目を実装する
+	return (
+		<div className='button-container'>
+			<span>{label}</span>
+			<button disabled={disabled} onClick={onClick}>
+				{text}
+			</button>
+		</div>
+	)
+}
