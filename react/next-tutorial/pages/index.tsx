b551aaceb34d628349b4411b7ac0198f7f035441
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-05-02 15:43:42 +0900

    Next.jsのコンポーネントにスタイルを使用 ThemeProviderを使いテーマを作成
    Theme.tsを作成
    作成したテーマをThemeProviderに渡す

--- react/next-tutorial/pages/index.tsx
+++ react/next-tutorial/pages/index.tsx
@@ -1,24 +1,42 @@
-import type { NextPage } from 'next'
-import styled, { css } from 'styled-components'
+import { NextPage } from 'next'
+import Link, { LinkProps } from 'next/link'
+import styled from 'styled-components'
+// Next.jsのコンポーネントにスタイルを使用
 
-// 青いボールド文字を表示するコンポーネント
-const Text = styled.p`
-	color: blue;
-	font-weight: bold;
+type BaseLinkProps = React.PropsWithChildren<LinkProps> & {
+	className?: string
+	children: React.ReactNode
+}
+
+// Next.jsのリンクにスタイルを適用するためのヘルパーコンポーネント
+// このコンポーネントをstyled-componentsで使用すると、定義したスタイルに対応するclassNameがpropsとして渡される
+// このclassNameをa要素に渡す
+const BaseLink = (props: BaseLinkProps) => {
+	const { className, children, ...rest } = props
+	return (
+		<Link {...rest}>
+			<a className={className}>{children}</a></Link>
+	)
+}
+
+const Text = styled.span`
+	/* themeから値を参照してスタイルを適用 */
+	color: ${(props) => props.theme.colors.red};
+	font-size: ${(props) => props.theme.fontSizes[3]};
+	margin: ${(props) => props.theme.space[2]};
 `
 
-// Textを継承し、ボーダーのスタイルを加えたコンポーネント
-const BorderedText = styled(Text)`
-	padding: 8px 16px;
-	border: 3px solid blue;
-	border-radius: 8px;
+const StyledLink = styled(BaseLink)`
+	color: #1e90ff;
+	font-size: 2em;
 `
 
 const Page: NextPage = () => {
 	return (
 		<div>
-			<Text>Hello</Text>
-			<BorderedText>World</BorderedText>
+			{/* 青色のリンクを表示する */}
+			<StyledLink href="/">Go to index</StyledLink>
+			<Text>Themeから参照した色を使用しています。</Text>
 		</div>
 	)
 }
