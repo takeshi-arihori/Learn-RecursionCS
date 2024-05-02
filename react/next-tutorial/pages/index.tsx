c5db06b64f5a7ee879343b68de7618f28435a33c
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-05-02 14:37:54 +0900

    スタイルの継承

--- react/next-tutorial/pages/index.tsx
+++ react/next-tutorial/pages/index.tsx
@@ -1,43 +1,24 @@
 import type { NextPage } from 'next'
 import styled, { css } from 'styled-components'
 
-// 赤色のボーダーを表示するスタイル
-const redBox = css`
-	padding: 0.25em 1em;
-	border: 3px solid #ff0000;
-	border-radius: 10px;
-`
-
-// 青色文字を表示するスタイル
-const font = css`
-	color: #1e90ff;
-	font-size: 2em;
-`
-
-// 赤色ボーダーと青色文字のスタイルをそれぞれ適用し、背景が透明なボタンコンポーネント
-const Button = styled.button`
-	background: transparent;
-	margin: 1em;
-	cursor: pointer;
-
-	${redBox}
-	${font}
-`
-
-// 青色文字のスタイルを継承し、ボールドでテキストを表示するコンポーネント
+// 青いボールド文字を表示するコンポーネント
 const Text = styled.p`
+	color: blue;
 	font-weight: bold;
+`
 
-	${font}
+// Textを継承し、ボーダーのスタイルを加えたコンポーネント
+const BorderedText = styled(Text)`
+	padding: 8px 16px;
+	border: 3px solid blue;
+	border-radius: 8px;
 `
 
 const Page: NextPage = () => {
 	return (
 		<div>
-			{/* 青色文字で赤色ボーダーのボタンを表示 */}
-			<Button>Hello</Button>
-			{/* 青色文字のテキストを表示 */}
-			<Text>World</Text>
+			<Text>Hello</Text>
+			<BorderedText>World</BorderedText>
 		</div>
 	)
 }
