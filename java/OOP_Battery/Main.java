d5733bdb4c1ab69a1a585b70206ea6c99d355a60
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-05-14 19:50:44 +0900

    private getter setter の設定

--- java/OOP_Battery/Main.java
+++
@@ -1,5 +0,0 @@
-package OOP_Battery;
-
-public class Main {
-
-}
---
+++ java_test/OOP_Battery/ExternalModule.java
@@ -0,0 +1,13 @@
+package OOP_Battery;
+
+public class ExternalModule {
+	public static void dangerousMethod(String customerId, Battery7v battery) {
+		System.out.println("Processing data....internals");
+		System.out.println("Client " + customerId + " purchased a " + battery.toString());
+		// battery.manufacturedCount += 4234;
+	}
+
+	public static void otherDangerousMethod() {
+		// Battery7v.manufacturedCount += 10000;
+	}
+}
