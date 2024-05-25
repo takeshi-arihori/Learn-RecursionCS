3e82d7914a03b1aeef1f68b61b1690c9cbf9b907
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-05-25 10:37:26 +0900

    単一責任の原則に従うために、Gear クラスの責任を分割

--- ruby/practice_guide_OOP/main.rb
+++ ruby/practice_guide_OOP/main.rb
@@ -1,25 +1,38 @@
 class Gear
-	attr_reader :chainring, :cog, :rim, :tire
-	def initialize(chainring, cog, rim, tire)
+	attr_reader :chainring, :cog
+	def initialize(chainring, cog)
 		@chainring = chainring
 		@cog = cog
-		@rim = rim
-		@tire = tire
 	end
 
 	# ギア比を計算する
 	def ratio
 		chainring / cog.to_f
 	end
+end
+
+class Wheel
+	attr_reader :rim, :tire
+	def initialize(rim, tire)
+		@rim = rim
+		@tire = tire
+	end
+
+	# リムとタイヤの直径を計算する
+	def diameter
+		rim + (tire * 2)
+	end
 
 	# ギアインチを計算する
-	def gear_inches
-		# タイヤはリムの周りを囲むので、直径を計算するためには2倍にする
-		ratio * (rim + (tire * 2))
+	def gear_inches(gear)
+		gear.ratio * diameter
 	end
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches # 137.0909090909091
-puts Gear.new(52, 11, 24, 1.25).gear_inches # 125.27272727272728
+gear = Gear.new(52, 11)
+wheel = Wheel.new(26, 1.5)
+puts wheel.gear_inches(gear) # 137.0909090909091
 
-puts Gear.new(52, 11).ratio # ArgumentError (wrong number of arguments (given 2, expected 4))
+gear = Gear.new(52, 11)
+wheel = Wheel.new(24, 1.25)
+puts wheel.gear_inches(gear) # 125.27272727272728
