18e0ba035a7198772c3a91b3338bbc9854d2b665
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-05-25 10:33:41 +0900

    複数の責任を持つクラスを作成 現在の Gear クラスは以下の責任を持っています：
    初期化時にチェーンリング、コグ、リム、タイヤの値を設定する責任
    ギア比を計算する責任（ratio メソッド）
    ギアインチを計算する責任（gear_inches メソッド）
    単一責任の原則
    単一責任の原則に従うためには、クラスは一つの責任だけを持つべきです。この責任とは、変更の理由が一つしかないということです。
    
    Gear クラスは、ギアの計算に関する複数の責任を持っています。このクラスは以下の2つの異なる計算責任を持っていると言えます：
    
    ギア比を計算する責任（ratio メソッド）
    ギアインチを計算する責任（gear_inches メソッド）
    これらの責任は密接に関連しているように見えますが、実際には別々に変更される可能性があります。例えば、ギアインチの計算方法が変更される場合、その変更はギア比の計算には影響を与えないはずです。

--- ruby/practice_guide_OOP/main.rb
+++ ruby/practice_guide_OOP/main.rb
@@ -1,16 +1,25 @@
 class Gear
-	attr_reader :chainring, :cog
-	def initialize(chainring, cog)
+	attr_reader :chainring, :cog, :rim, :tire
+	def initialize(chainring, cog, rim, tire)
 		@chainring = chainring
 		@cog = cog
+		@rim = rim
+		@tire = tire
 	end
 
+	# ギア比を計算する
 	def ratio
 		chainring / cog.to_f
 	end
+
+	# ギアインチを計算する
+	def gear_inches
+		# タイヤはリムの周りを囲むので、直径を計算するためには2倍にする
+		ratio * (rim + (tire * 2))
+	end
 end
 
+puts Gear.new(52, 11, 26, 1.5).gear_inches # 137.0909090909091
+puts Gear.new(52, 11, 24, 1.25).gear_inches # 125.27272727272728
 
-puts Gear.new(52, 11).ratio # => 4.7272727272727275
-puts Gear.new(30, 27).ratio # => 1.1111111111111112
-
+puts Gear.new(52, 11).ratio # ArgumentError (wrong number of arguments (given 2, expected 4))
