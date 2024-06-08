7e0ced4e488186575ec2e01d45ab094c5b5583bb
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-06-08 17:42:05 +0900

    linux環境でのcomposer install完了

--- dynamic-web-server/src/index.php
+++ dynamic-web-server/src/index.php
@@ -1,18 +1,4 @@
-<?php $planet = isset($_GET['planet']) ? $_GET['planet'] : 'Earth' ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <title>Hello, World!</title>
-</head>
-
-<body>
-  <h1>Hello, <?php echo $planet; ?></h1>
-  <p>This is a simple "Hello, <?php echo $planet; ?>!" HTML page.</p>
-
-  <footer>
-    <p><?php echo sprintf("%s Rocks©  Website %s. All rights reserved.", $planet, date("Y")); ?></p>
-  </footer>
-</body>
-
-</html>
+<?php
+header('Content-Type: application/json');
+echo json_encode(get_headers() ?? '{}');
+?>
