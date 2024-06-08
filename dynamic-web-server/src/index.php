89373f1460c4b24796347f1a4b0cc111a8272c77
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-06-08 15:49:14 +0900

    HTTPデータの取得 $_GET

--- dynamic-web-server/src/index.php
+++ dynamic-web-server/src/index.php
@@ -1,4 +1,18 @@
-<?php
+<?php $planet = isset($_GET['planet']) ? $_GET['planet'] : 'Earth' ?>
+<!DOCTYPE html>
+<html lang="en">
 
-// テスト
-echo "Hello World!";
+<head>
+  <title>Hello, World!</title>
+</head>
+
+<body>
+  <h1>Hello, <?php echo $planet; ?></h1>
+  <p>This is a simple "Hello, <?php echo $planet; ?>!" HTML page.</p>
+
+  <footer>
+    <p><?php echo sprintf("%s Rocks©  Website %s. All rights reserved.", $planet, date("Y")); ?></p>
+  </footer>
+</body>
+
+</html>
