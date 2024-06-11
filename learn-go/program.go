d858af1b4597ff0e62e42b8ac62402e8b2b7cc96
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-06-11 10:33:15 +0900

    Go Learning
    go run program.go
    コンパイルと実行: go runコマンドは、指定されたGoソースファイル（program.go）を一時的にコンパイルし、直ちにそのバイナリを実行します。
    利便性: 簡単にソースコードをテストしたい場合や、開発中のプログラムをすぐに動かしたい場合に便利です。
    ファイル生成: 一時的なバイナリファイルを生成しますが、通常はこのファイルを保持せず、実行後に削除されます。
    
    go build program.go
    コンパイル: go buildコマンドは、指定されたGoソースファイル（program.go）をコンパイルして実行可能なバイナリファイルを生成します。
    永続的なバイナリ: 生成されたバイナリファイルは、ディスク上に保存され、後で繰り返し実行できます。
    配布: 他の環境やシステムにプログラムを配布するために、バイナリファイルを作成する際に使用されます。

---
+++ learn-go/program.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+func processData(Data []int){
+		// Process data
+		fmt.Println(Data)
+}
+
+func main(){
+		// Data
+		Data := []int{1,2,3,4,5}
+		// Call function
+		processData(Data)
+}
+
