af30c3704504245143dd4f2bd7c218446e7e4208
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-06-13 23:44:54 +0900

    Go data 学習

--- learn-go/program.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	// fmt.Printf("The value of x is: %d", 42) // The value of x is: 42
-  // fmt.Printf("The value of pi is approximately: %.2f\n", 3.14159)
-	fmt.Printf("Hello\n%s", "World")
-}
