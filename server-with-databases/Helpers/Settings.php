d13a48b7f703dc39ee3eb6aed1a78169817d3e65
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-06-17 00:48:39 +0900

    DDL操作
    - [spl_autoload_registerのエラー修正](https://github.com/takeshi-arihori/til/blob/main/php/spl_autoload_register.md)

--- server-with-databases/Helpers/Settings.php
+++ server-with-databases/Helpers/Settings.php
@@ -4,19 +4,17 @@ namespace Helpers;
 
 use Exceptions\ReadAndParseEnvException;
 
-class Settings {
-  private const ENV_FILE = '.env';
+class Settings{
+    private const ENV_PATH =  '.env';
 
-  public static function env(string $pair): string{
-        // dirname関数は、指定されたファイルパスの親ディレクトリのパスを返す関数です。
-        // この関数には「levels」という整数型のパラメータを設定することができ、これは「いくつ親ディレクトリを遡るか」を指定するものです。
-        // デフォルトではこの「levels」は1に設定されており、つまり、ファイルの直接の親ディレクトリのパスを返します。
-        $config = parse_ini_file(dirname(__DIR__, 2) . '/' . self::ENV_FILE);
+    public static function env(string $pair): string{
+        // dirname関数は、指定されたファイルパスの親ディレクトリのパスを返す関数です。この関数には「levels」という整数型のパラメータを設定することができ、これは「いくつ親ディレクトリを遡るか」を指定するものです。デフォルトではこの「levels」は1に設定されており、つまり、ファイルの直接の親ディレクトリのパスを返します。
+        $config = parse_ini_file( dirname(__FILE__, 2) . '/' . self::ENV_PATH);
 
         if($config === false){
-            throw new ReadAndParseEnvException('Error reading or parsing the .env file');
+            throw new ReadAndParseEnvException();
         }
 
         return $config[$pair];
-  }
+    }
 }
--- server-with-databases/learn.md
+++ server-with-databases/learn.md
@@ -7,3 +7,19 @@ sudo service mysql start
 sudo service mysql status
 sudo service mysql stop
 ```
+
+## DDL (Data Definition Language) 操作
+データ定義言語: テーブルや索引、シーケンスなどのデータベースオブジェクトを定義する言語。
+
+1つのDBが1つのアプリケーションと紐付いている
+
+### エントリーポイントの読み込み
+```
+spl_autoload_register( function($name) {
+  // __DIR__は、現在のファイルの絶対ディレクトリパスを取得します。
+  $filepath = __DIR__ . "/" . str_replace('\\', '/', $name) . ".php";
+  echo "\nRequiring...." . $name . " once ($filepath).\n";
+  // バックスラッシュ(\)をフロントスラッシュ(/)に置き換えます。フロントスラッシュはLinuxのファイルパスで使用されます。
+  require_once $filepath;
+});
+```
