e269932c2e164192ded6940b07ec44c13f66bcc3
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-06-16 23:36:38 +0900

    mysqliクラスの設定

--- server-with-databases/init-app.php
+++ server-with-databases/init-app.php
@@ -3,7 +3,34 @@ spl_autoload_extensions('.php');
 spl_autoload_register();
 
 use Helpers\Settings;
+/**
+ * 接続の失敗時にエラーを報告し、例外をスローします。
+ * データベース接続を初期化する前にこの設定を行ってください。
+ * テストするには、.env設定で誤った情報を入力します。
+ */
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
+/*
+ * https://www.php.net/manual/en/class.mysqli.php で利用可能なすべてのメソッドを確認できます。
+ */
+$mysqli = new mysqli('localhost', Settings::env('DATABASE_USER'), Settings::env('DATABASE_USER_PASSWORD'), Settings::env('DATABASE_NAME'));
 
-printf("Local database username: %s\n", Settings::env('DATABASE_USER'));
-printf("Local database password (hashed): %s\n", password_hash(Settings::env('DATABASE_USER_PASSWORD'), PASSWORD_DEFAULT));
+// https://www.php.net/manual/en/mysqli.get-charset.php
+$charset = $mysqli->get_charset();
+
+if($charset === null) throw new Exception('Charset could be read');
+
+printf(
+  "%s's charset: %s.%s",
+  Settings::env('DATABASE_NAME'),
+  $charset->charset,
+  PHP_EOL
+);
+
+printf(
+  "collation: %s.%s",
+  $charset->collation,
+  PHP_EOL
+);
+
+$mysqli->close();
--- server-with-databases/learn.md
+++ server-with-databases/learn.md
@@ -1,1 +1,9 @@
-## サーバとデータ層
+## mysqliクラス
+
+### MySQLのサービス開始・再起動・停止・コマンド
+
+```
+sudo service mysql start
+sudo service mysql status
+sudo service mysql stop
+```
