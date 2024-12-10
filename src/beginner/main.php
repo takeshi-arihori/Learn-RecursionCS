8e1d7894b286b3ae8e695177f3306c56b267ad3a
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-12-10 22:20:25 +0900

    convert_to_century

--- src/beginner/main.php
+++ src/beginner/main.php
@@ -1,3 +1,8 @@
+<?php
+// Include the function and execute if a year is provided
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,52 +23,8 @@
 
     <div class="container mx-auto">
         <main class="text-center mb-2 h-[calc(100vh-3rem)]">
-
-            <h1>西暦を世紀へ変換</h1>
-            <p>medium</p>
-            <div>西暦 year が与えられるので、世紀へと変更する関数、getCentury という関数を作成してください。</div>
             <?php
-            function getCentury(int $year): string
-            {
-                // 2で終わる場合はnd（例：22nd, 32nd）
-                // 3で終わる場合はrd（例：23rd, 33rd）
-                // それ以外はth（例：24th, 25th）
-                // 数字が11, 12, 13の場合はthを頭につける
-                $suffix = "th";
-                $century = ceil($year / 100);
-
-                // echo "century: " . $century . PHP_EOL;   
-                // echo "century % 100: " . $century % 100 . PHP_EOL;   
-                // echo "century % 10: " . $century % 10 . PHP_EOL;   
-
-                if (!in_array($century % 100, [11, 12, 13])) {
-                    // 1で終わる場合はst（例：21st, 31st）
-                    switch ($century % 10) {
-                        case 1:
-                            $suffix = "st";
-                            break;
-                        case 2:
-                            $suffix = "nd";
-                            break;
-                        case 3:
-                            $suffix = "rd";
-                            break;
-                        default:
-                            $suffix = "th";
-                    }
-                }
-
-                return $century . $suffix . " century";
-            }
-
-            echo getCentury(2000) . '<br>';
-            echo getCentury(2001) . '<br>';
-            echo getCentury(2002) . '<br>';
-            echo getCentury(101) . '<br>';
-            echo getCentury(23) . '<br>';
-            echo getCentury(3301) . '<br>';
-
-
+            require_once('convert_to_century.php');
             ?>
         </main>
 
