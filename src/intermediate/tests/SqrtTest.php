a0dc65a214735ac09bd868198629aa23d535c8d3
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-12-25 07:22:38 +0900

    バビロンの平方根

---
+++ src/intermediate/tests/SqrtTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../sqrt.php';
+
+class SqrtTest extends TestCase
+{
+    public function testIsSquareRootCloseEnough()
+    {
+        $this->assertTrue(isSquareRootCloseEnough(1.41421356, 1.41421356));
+        $this->assertTrue(isSquareRootCloseEnough(1.41421356, 1.41421357));
+        $this->assertTrue(isSquareRootCloseEnough(1.41421356, 1.41421358));
+    }
+
+    public function testSquareRootHelper()
+    {
+        $this->assertEqualsWithDelta(1.41421356, squareRootHelper(2, 1.4), 0.000001);
+        $this->assertEqualsWithDelta(1.41421356, squareRootHelper(2, 1.414285), 0.000001);
+    }
+}
