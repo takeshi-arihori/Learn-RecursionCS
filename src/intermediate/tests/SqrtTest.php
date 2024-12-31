9e1c0a8e25bc33adbb5d0e1803baa5aeaf177d62
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-01-01 07:49:29 +0900

    whileでsqrtのテスト

--- src/intermediate/tests/SqrtTest.php
+++ src/intermediate/tests/SqrtTest.php
@@ -18,6 +18,15 @@ class SqrtTest extends TestCase
         $this->assertEqualsWithDelta(1.41421356, squareRootHelper(2, 1.4), 0.000001);
         $this->assertEqualsWithDelta(1.41421356, squareRootHelper(2, 1.414285), 0.000001);
     }
+
+    public function testSquareRootWhile()
+    {
+        $this->assertEqualsWithDelta(1.41421356, squareRootWhile(2), 0.000001);
+        $this->assertEqualsWithDelta(1.73205081, squareRootWhile(3), 0.000001);
+        $this->assertEqualsWithDelta(2.0, squareRootWhile(4), 0.000001);
+        $this->assertEqualsWithDelta(2.23606798, squareRootWhile(5), 0.000001);
+        $this->assertEqualsWithDelta(3.0, squareRootWhile(9), 0.000001);
+    }
 }
 
 // 実行方法
