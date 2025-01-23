bbd7599da44b6683b9d8e3a1b7775339e8394952
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-01-24 04:42:39 +0900

    update: useState nest object

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -70,6 +70,7 @@ const App = () => {
             <MyInput />
             <Form />
             <FormObject />
+            <FormObjectNested />
         </StyleWrapper>
     )
 }
@@ -178,6 +179,7 @@ const Form = () => {
     );
 }
 
+// フォーム
 const FormObject = () => {
     const [form, setForm] = useState({
         firstName: '',
@@ -225,6 +227,100 @@ const FormObject = () => {
                 {form.lastName}{' '}
                 ({form.email})
             </p>
+        </ElementWrap>
+    );
+}
+
+// フォーム ネストされたオブジェクト
+const FormObjectNested = () => {
+    const [person, setPerson] = useState({
+        name: 'Niki de Saint Phalle',
+        artwork: {
+            title: 'Blue Nana',
+            city: 'Hamburg',
+            image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+        }
+    });
+
+    function handleNameChange(e) {
+        setPerson({
+            ...person,
+            name: e.target.value
+        })
+    }
+
+    function handleTitleChange(e) {
+        setPerson({
+            ...person,
+            artwork: {
+                ...person.artwork,
+                title: e.target.value
+            }
+        });
+    }
+
+    function handleCityChange(e) {
+        setPerson({
+            ...person,
+            artwork: {
+                ...person.artwork,
+                city: e.target.value
+            }
+        });
+    }
+
+    function handleImageChange(e) {
+        setPerson({
+            ...person,
+            artwork: {
+                ...person.artwork,
+                image: e.target.value
+            }
+        });
+    }
+
+    return (
+        <ElementWrap>
+            <Title>Form Object Nested</Title>
+            <label>
+                Name:
+                <input
+                    value={person.name}
+                    onChange={handleNameChange}
+                />
+            </label>
+            <label>
+                Title:
+                <input
+                    value={person.artwork.title}
+                    onChange={handleTitleChange}
+                />
+            </label>
+            <label>
+                City:
+                <input
+                    value={person.artwork.city}
+                    onChange={handleCityChange}
+                />
+            </label>
+            <label>
+                Image:
+                <input
+                    value={person.artwork.image}
+                    onChange={handleImageChange}
+                />
+            </label>
+            <p>
+                <i>{person.artwork.title}</i>
+                {' by '}
+                {person.name}
+                <br />
+                (located in {person.artwork.city})
+            </p>
+            <img
+                src={person.artwork.image}
+                alt={person.artwork.title}
+            />
         </ElementWrap>
     );
 }
