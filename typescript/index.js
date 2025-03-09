5c73e7c93f70c9b6e897a24b5b8873d7f8ff4d8a
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-03-09 22:04:08 +0900

    add: typeScript tutorial

---
+++ typescript/index.js
@@ -0,0 +1,6 @@
+var message = "Hello, TypeScript!";
+console.log(message);
+var username = "John";
+console.log(username);
+var age = 25;
+console.log(age);
---
+++ typescript/index.ts
@@ -0,0 +1,8 @@
+let message: string = "Hello, TypeScript!";
+console.log(message);
+
+let username = "John";
+console.log(username);
+
+let age = 25;
+console.log(age);
