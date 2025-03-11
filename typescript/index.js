7649f63581c136baf0a8df40d4af0079720392a0
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-03-11 21:30:10 +0900

    delete:unnecessary code

--- typescript/index.js
+++
@@ -1,6 +0,0 @@
-var message = "Hello, TypeScript!";
-console.log(message);
-var username = "John";
-console.log(username);
-var age = 25;
-console.log(age);
--- typescript/index.ts
+++
@@ -1,8 +0,0 @@
-let message: string = "Hello, TypeScript!";
-console.log(message);
-
-let username = "John";
-console.log(username);
-
-let age = 25;
-console.log(age);
