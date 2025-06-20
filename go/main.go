c2c9d00b0590508024d1932bdb0ca24f930b60fd
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-06-21 07:51:11 +0900

    add: go function arguments types

--- go/main.go
+++ go/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
-	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println(add(42, 13))
 }
