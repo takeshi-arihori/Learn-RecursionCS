3f91da75c4925175748a26b208da828b888ba7e2
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-06-22 19:49:24 +0900

    add: プリミティブ型

---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module recursionCurriculum
+
+go 1.24.4
--- go/main.go
+++ go/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 )
 
-func add(x int, y int) int {
-	return x + y
-}
+type Feet float64
 
 func main() {
-	fmt.Println(add(42, 13))
+	var str string = "Hello, World!"
+	var i int
+	var num int64 = 2147483648
+	var x uintptr = uintptr(unsafe.Pointer(&i))
+	var xfloat Feet = 5.5
+	fmt.Println("Address of i:", x)
+	fmt.Println("Value of num:", num)
+	fmt.Println("Size of int:", unsafe.Sizeof(i))
+	fmt.Println("Size of string:", unsafe.Sizeof(str))
+	fmt.Println("Size of Feet:", unsafe.Sizeof(xfloat))
 }
